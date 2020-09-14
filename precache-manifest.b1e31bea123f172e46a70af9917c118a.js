4ee895fb1d2b7cb302f3be63f640058e616c986c
Oleksandr Fihun <oleksandr.fihun@globallogic.com>
2020-09-14 16:08:22 +0300

    Updates

---
+++ precache-manifest.b1e31bea123f172e46a70af9917c118a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32c81ed398194a5aae5486284aeffb8c",
+    "url": "/react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "00c32a6c929ee6394cd9",
+    "url": "/react-hw-02-phonebook/static/css/main.4aba9d7b.chunk.css"
+  },
+  {
+    "revision": "ed9b559089bd1b312e3f",
+    "url": "/react-hw-02-phonebook/static/js/2.ac689fdc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hw-02-phonebook/static/js/2.ac689fdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00c32a6c929ee6394cd9",
+    "url": "/react-hw-02-phonebook/static/js/main.e0f56d7d.chunk.js"
+  },
+  {
+    "revision": "6f482f3e5b3d66793c08",
+    "url": "/react-hw-02-phonebook/static/js/runtime-main.2c5ed82f.js"
+  }
+]);
